ece49b03820a6f208040b559b51239d6c75fe4f5
Anthony Paddison <anthony.paddison@innserveltd.com>
2025-09-27 21:58:07 +0100

    HA - WIP

--- www/calendarHandler.js
+++ www/calendarHandler.js
@@ -32,6 +32,7 @@
 
   function styleLegendItem(el) {
     console.log('styleLegendItem:', el);
+    console.log('styleLegendItem:', el.text());
 
     if (!el || el.nodeType !== 1 || el.getAttribute(state.appliedAttr)) return;
     for (const [prop, value] of PROPS) {
